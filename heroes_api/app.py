9ca37a5ae813d3f71cbc150b23746b6a82a7d628
Lucas Moreira <lucas-mopi@htomail.com>
2022-08-18 09:05:03 -0300

    Call start socket when start app

--- heroes_api/app.py
+++ heroes_api/app.py
@@ -1,14 +1,18 @@
 """Start Flsk Module"""
 # pylint: disable=unused-import,wrong-import-position
-from flask import Flask
 from flask_bcrypt import Bcrypt
-
+from flask import Flask
 from models.base import db
 from utils import create_app, init_database
+from services.listener import socket_heroes
+from config import ENVIRONMENT
 
-app = create_app(db)
+app: Flask = create_app(db)
 init_database(app, db)
 # bcrypt = Bcrypt(app)
+
+if ENVIRONMENT != 'test':
+    socket_heroes.start_socketio_connection(app)
 
 import routes.auth
 import routes.hero

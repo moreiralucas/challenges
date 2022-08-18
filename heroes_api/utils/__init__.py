2df15ee7c163cc1d32730996d21ff5894362c2aa
Lucas Moreira <lucas-mopi@htomail.com>
2022-08-18 09:03:38 -0300

    Configure init database

--- heroes_api/utils/__init__.py
+++ heroes_api/utils/__init__.py
@@ -2,4 +2,7 @@
 from .initialization import create_app
 from .database import init_database
 
-__all__ = ["create_app", "init_database",]
+__all__ = [
+    "create_app",
+    "init_database",
+]
--- heroes_api/utils/database.py
+++ heroes_api/utils/database.py
@@ -3,8 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from config import ENVIRONMENT
 
+
 def init_database(app: Flask, database: SQLAlchemy):
     """Create all models of database"""
-    if ENVIRONMENT != 'test':
+    if ENVIRONMENT != "test":
         with app.app_context():
             database.create_all()
